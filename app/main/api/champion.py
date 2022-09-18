fb0286c60b1abeb7bdfb33f6c480a4655231c98c
seungyong <44765636+seungyong@users.noreply.github.com>
2022-09-18 17:41:37 +0900

    Champion insert, delete 추가
    1. insert, delete 함수 추가
    
    1-1. API가 아닌 서버 단에서 사용할 코드 목적으로 생성

--- app/main/api/champion.py
+++ app/main/api/champion.py
@@ -1,10 +1,11 @@
 from urllib import request
 from json import loads
 
-from flask import Blueprint, request
+from flask import Blueprint
 from flask_restx import Namespace, Resource, fields
 from app import session
 from ..models.champion import Champion as Champion_model, response_all_model, response_name_model
+from ..models.champion_skill import ChampionSkill as ChampionSkill_model
 from ..models.champion_skill import response_model as champion_skill_response_model
 
 from .champion_skill import insert_champions_skill, get_champion_skill
@@ -34,6 +35,106 @@ response_name_model = champion_ns.model('Champion Name Information', {
     'results': fields.List(fields.String(response_name_model())),
     'statusCode': fields.Integer(200)
 })
+
+
+def insert_champions(champion_names):
+    try:
+        champions = []
+        champions_skills = []
+        version = version_util.get_version()
+
+        for champion in champion_names:
+            url = riot_url.champion_url(version, champion)
+
+            with request.urlopen(url) as res:
+                data = loads(res.read().decode())
+                info = data['data'][champion]
+
+                champions.append(Champion_model(
+                    champion_id=info['key'],
+                    champion_name=info['name'],
+                    eng_name=info['id'],
+                    sub_name=info['title'],
+                    description=info['blurb'],
+                    position='TOP',
+                    tags=', '.join(info['tags']),
+                    difficulty=info['info']['difficulty'],
+                    hp=info['stats']['hp'],
+                    hp_per_level=info['stats']['hpperlevel'],
+                    mp=info['stats']['mp'],
+                    mp_per_level=info['stats']['mpperlevel'],
+                    move_speed=info['stats']['movespeed'],
+                    armor=info['stats']['armor'],
+                    armor_per_level=info['stats']['armorperlevel'],
+                    attack_range=info['stats']['attackrange'],
+                    attack_damage=info['stats']['attackdamage'],
+                    attack_damage_per_level=info['stats']['attackdamageperlevel'],
+                    attack_speed=info['stats']['attackspeed'],
+                    attack_speed_per_level=info['stats']['attackspeedperlevel'],
+                ))
+
+                champions_skills.append(ChampionSkill_model(
+                    champion_id=info['key'],
+                    p_name=info['passive']['name'],
+                    p_description=info['passive']['description'],
+                    p_thumbnail=info['passive']['image']['full'],
+                    q_id=info['spells'][0]['id'],
+                    q_name=info['spells'][0]['name'],
+                    q_description=info['spells'][0]['description'],
+                    q_tooltip=info['spells'][0]['tooltip'],
+                    q_cooldown=info['spells'][0]['cooldownBurn'],
+                    q_cost=info['spells'][0]['costBurn'],
+                    q_range=info['spells'][0]['rangeBurn'],
+                    q_thumbnail=info['spells'][0]['image']['full'],
+                    w_id=info['spells'][1]['id'],
+                    w_name=info['spells'][1]['name'],
+                    w_description=info['spells'][1]['description'],
+                    w_tooltip=info['spells'][1]['tooltip'],
+                    w_cooldown=info['spells'][1]['cooldownBurn'],
+                    w_cost=info['spells'][1]['costBurn'],
+                    w_range=info['spells'][1]['rangeBurn'],
+                    w_thumbnail=info['spells'][1]['image']['full'],
+                    e_id=info['spells'][2]['id'],
+                    e_name=info['spells'][2]['name'],
+                    e_description=info['spells'][2]['description'],
+                    e_tooltip=info['spells'][2]['tooltip'],
+                    e_cooldown=info['spells'][2]['cooldownBurn'],
+                    e_cost=info['spells'][2]['costBurn'],
+                    e_range=info['spells'][2]['rangeBurn'],
+                    e_thumbnail=info['spells'][2]['image']['full'],
+                    r_id=info['spells'][3]['id'],
+                    r_name=info['spells'][3]['name'],
+                    r_description=info['spells'][3]['description'],
+                    r_tooltip=info['spells'][3]['tooltip'],
+                    r_cooldown=info['spells'][3]['cooldownBurn'],
+                    r_cost=info['spells'][3]['costBurn'],
+                    r_range=info['spells'][3]['rangeBurn'],
+                    r_thumbnail=info['spells'][3]['image']['full'],
+                ))
+
+        session.add_all(champions)
+        session.add_all(champions_skills)
+        session.commit()
+
+        return 204
+    except Exception:
+        session.rollback()
+        return 500
+
+
+def delete_champions(champion_names):
+    try:
+        # cascade 때문에 skills도 같이 삭제됨.
+        session.query(Champion_model).filter(Champion_model.eng_name.in_(tuple(champion_names))).delete()
+        session.commit()
+
+        status_code = 204
+    except Exception as e:
+        print(e)
+        status_code = 500
+        session.rollback()
+
+    return status_code
 
 
 @champion_ns.route('/')
@@ -208,4 +309,3 @@ class ChampionName(Resource):
                 return {'message': 'Internal Server Error'}, 500
         else:
             return {'message': 'Bad Request'}, 400
-        
